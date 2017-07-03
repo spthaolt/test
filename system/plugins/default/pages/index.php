1bbcb774b669f8ae6e460b3350f73f4f664f82a0
Thao le <thaolt@songphi.com>
2017-07-03 22:27:53 +0700

    full screen background

--- system/plugins/default/pages/index.php
+++ system/plugins/default/pages/index.php
@@ -10,4 +10,4 @@
  */
 $title = ossn_print('site:index');
 $content = ossn_set_page_layout('startup', array('content' => ossn_plugin_view('pages/contents/index')));
-echo ossn_view_page($title, $content);
+echo ossn_view_page($title, $content,'startup');
