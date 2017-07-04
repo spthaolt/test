df2b69adad780f90cf8f8e48bbc6cb58af74dd26
Thao le <thaolt@songphi.com>
2017-07-04 15:16:17 +0700

    corrected smiley replacing

--- components/OssnSmilies/libraries/smilify.lib.php
+++ components/OssnSmilies/libraries/smilify.lib.php
@@ -4,48 +4,35 @@
  */
 function smilify($text) {
 
-    $ascii_pattern = array(
-            ':(',
-            ':)',
-            '=D',
-            ';)',
-            ':p',
-            '8|',
-            'o.O',
-            ':O',
-            ':*',
-            'a:',
-            ':h:',
-            '3:|',
-            'u:',
-            ':v',
-            'g:',
-            '8)',
-            'c:'
-    );
-    
     $icon = ossn_site_url() . 'components/OssnChat/images/emoticons/';
-    $smiley_icon = array(
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-sad.gif' />",
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-smile.gif' />",
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-happy.gif' />",
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-wink.gif' />",
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-tongue.gif' />",
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-sunglasses.gif' />",
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-confused.gif' />",
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-gasp.gif' />",
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-kiss.gif' />",
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-angel.gif' />",
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-heart.gif' />",
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-devil.gif' />",
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-upset.gif' />",
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-pacman.gif' />",
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-grumpy.gif' />",
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-glasses.gif' />",
-            "<img class='ossn-smiley-item' src='{$icon}ossnchat-cry.gif' />"
+    
+    $ascii_pattern = array(
+            ':(' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-sad.gif' />",
+            ':)' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-smile.gif' />",
+            '=D' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-happy.gif' />",
+            ';)' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-wink.gif' />",
+            ':p' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-tongue.gif' />",
+            '8|' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-sunglasses.gif' />",
+            'o.O' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-confused.gif' />",
+            ':O' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-gasp.gif' />",
+            ':*' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-kiss.gif' />",
+            'a:' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-angel.gif' />",
+            ':h:' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-heart.gif' />",
+            '3:|' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-devil.gif' />",
+            'u:' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-upset.gif' />",
+            ':v' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-pacman.gif' />",
+            'g:' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-grumpy.gif' />",
+            '8)' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-glasses.gif' />",
+            'c:' => "<img class='ossn-smiley-item' src='{$icon}ossnchat-cry.gif' />"
     );
 
-    return str_replace($ascii_pattern, $smiley_icon, $text);
+
+    foreach($ascii_pattern as $smiley=>$image) {
+        $smiley = preg_quote($smiley);
+        $text = preg_replace("~\b$smiley\b~",$image,$text);
+    }    
+    
+    return $text;
 }
 
 
