732b80350694ab8a751290b35a4d88798f9cfcd7
Danh Huynh <danhhc@ezqua.com>
2017-07-06 14:03:23 +0700

    remove invite.js

--- components/OssnGroups/plugins/default/js/invite.php
+++
@@ -1,9 +0,0 @@
-
-Ossn.RegisterStartupFunction(function() {
-	$(document).ready(function() {
-		 $('#group-invite-check-all').click(function() {
-        	$aa = $(this).is(':checked');
-			alert($aa);
-		});
-	});
-});
