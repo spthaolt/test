bf14063a09c6cd2bad359c9d3cd0d1c6948069f1
Thao le <thaolt@songphi.com>
2017-07-10 13:48:53 +0700

    [vi] sociallogin and mobilelogin translation

---
+++ components/MobileLogin/locale/ossn.vi.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$vi = array(
+    'mobilelogin' => 'Mobile',
+    'mobilelogin:username' => 'Tên đăng nhập/Email/Số điện thoại',
+    'mobilelogin:invalid:mobile' => 'Số điện thoại không đúng',
+    'mobilelogin:num' => '+8490123456',
+    'mobilelogin:mobile:exists' => 'Số điện thoại đã được sử dụng',
+);
+ossn_register_languages('vi', $vi); 
---
+++ components/SocialLogin/locale/ossn.vi.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$vi = array(
+    'sociallogin' => 'Đăng nhập bằng Facebook',
+    'social:login:settings:saved' => 'Thiết lập đã được lưu',
+    'social:login:settings:save:error' => 'Không thể lưu thiết lập',
+    'social:login:app:secret' => 'APP Secret',
+    'social:login:app:key' => 'APP ID',
+    'social:login:facebook' => 'Facebook API details',
+    
+    'social:login:with:facebook' => 'Đăng nhập bằng tài khoản Facebook',
+    'social:login:account:create:error' => 'Có lỗi xảy ra, không thể đăng nhập bằng Facebook',
+    
+    'social:login:facebook:url' => 'OAuth redirect URI',
+    'social:login:fill:profile' => 'Vui lòng điền vào phần còn thiết trong hồ sơ của bạn trước',
+);
+ossn_register_languages('vi', $vi); 
