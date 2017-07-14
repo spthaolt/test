c082c2561368d501e5e155b93b9c92b665e9f361
Danh Huynh <danhhc@ezqua.com>
2017-07-14 14:18:44 +0700

    fixed only load blueimp js when #fileupload is existed

--- components/OssnWall/plugins/default/js/ossn_wall.php
+++ components/OssnWall/plugins/default/js/ossn_wall.php
@@ -162,133 +162,135 @@ Ossn.RegisterStartupFunction(function() {
 });
 
 Ossn.RegisterStartupFunction(function() {
-    $(document).ready(function() {
 
-        var process_url = "<?php echo ossn_site_url()?>postphotos/upload";
-        var delete_url = "<?php echo ossn_site_url()?>postphotos/delete";
+    if (typeof $('#fileupload') != 'undefined')
+        $(document).ready(function() {
 
-        $('#fileupload').fileupload({
-            url: process_url,
-            dataType: 'json',
-            autoUpload: true,
-            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
-            disableImageResize: /Android(?!.*Chrome)|Opera/
-                .test(window.navigator.userAgent),
-            previewMaxWidth: 100,
-            previewMaxHeight: 100,
-            previewCrop: true
-        }).on('fileuploadadd', function (e, data) {
+            var process_url = "<?php echo ossn_site_url()?>postphotos/upload";
+            var delete_url = "<?php echo ossn_site_url()?>postphotos/delete";
 
-            data.context = $('<div/>').addClass('file-wrapper').appendTo('#files');
+            $('#fileupload').fileupload({
+                url: process_url,
+                dataType: 'json',
+                autoUpload: true,
+                acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+                disableImageResize: /Android(?!.*Chrome)|Opera/
+                    .test(window.navigator.userAgent),
+                previewMaxWidth: 100,
+                previewMaxHeight: 100,
+                previewCrop: true
+            }).on('fileuploadadd', function (e, data) {
 
-            $.each(data.files, function (index, file) {
-                var node = $('<div/>').addClass('file-row');
+                data.context = $('<div/>').addClass('file-wrapper').appendTo('#files');
 
-                var hover  = $('<div style="display:none;cursor: pointer;"/>').addClass('upload-hover-remove');
-                var removeBtn  = $('<a/>').addClass('upload-remove').append('<i class="fa fa-times" aria-hidden="true"/>');
+                $.each(data.files, function (index, file) {
+                    var node = $('<div/>').addClass('file-row');
 
-                hover.append(removeBtn);
+                    var hover  = $('<div style="display:none;cursor: pointer;"/>').addClass('upload-hover-remove');
+                    var removeBtn  = $('<a/>').addClass('upload-remove').append('<i class="fa fa-times" aria-hidden="true"/>');
 
-                // show hile btn close
-                $(".file-wrapper").hover(function() {
-                    $(this).find(".upload-hover-remove").show();
-                }, function() {
-                    $(this).find(".upload-hover-remove").hide();
-                });   
+                    hover.append(removeBtn);
 
-                // event click
-                removeBtn.on('click', function(e, data){
-                    var imageId = $(this).parent().parent().find("#imageId").val();
+                    // show hile btn close
+                    $(".file-wrapper").hover(function() {
+                        $(this).find(".upload-hover-remove").show();
+                    }, function() {
+                        $(this).find(".upload-hover-remove").hide();
+                    });   
 
-                    Ossn.PostRequest({
-                        url: delete_url +"/"+ imageId,
-                        action: false,
-                        callback: function(callback) {
-                            
-                        }
+                    // event click
+                    removeBtn.on('click', function(e, data){
+                        var imageId = $(this).parent().parent().find("#imageId").val();
+
+                        Ossn.PostRequest({
+                            url: delete_url +"/"+ imageId,
+                            action: false,
+                            callback: function(callback) {
+                                
+                            }
+                        });
+                        $(this).parent().parent().remove();
                     });
-                    $(this).parent().parent().remove();
+
+                    node.append('<div class="circle"/>');
+                    node.append(hover);      
+                    node.appendTo(data.context);
                 });
 
-                node.append('<div class="circle"/>');
-                node.append(hover);      
-                node.appendTo(data.context);
-            });
-
-            $("#files").prepend(data.context);
-            $('.circle').progressCircle({
-                nPercent        : 0,
-                showPercentText : false,
-                circleSize      : 111,
-                thickness       : 3
-            });
-
-        }).on('fileuploadprocessalways', function (e, data) {
-            var index = data.index,
-                file = data.files[index],
-                node = $(data.context.children()[index]);
-
-            if (file.preview) {
-                node
-                    .prepend('<br>')
-                    .prepend(file.preview);
-            }
-            if (file.error) {
-                node
-                    .append('<br>')
-                    .append($('<span class="text-danger"/>').text(file.error));
-            }
-        }).on('fileuploadprogress', function (e, data) {
-            var progress = parseInt(data.loaded / data.total * 100, 10);
-            if (data.context) {
-                data.context.each(function () {
-                    $(this).find('.circle').children().first().progressCircle({ 
-                                                                nPercent : progress ,
-                                                                showPercentText : false,
-                                                                circleSize      : 111,
-                                                                thickness       : 3
-                                                            });
+                $("#files").prepend(data.context);
+                $('.circle').progressCircle({
+                    nPercent        : 0,
+                    showPercentText : false,
+                    circleSize      : 111,
+                    thickness       : 3
                 });
-            }
-        }).on('fileuploaddone', function (e, data) {
-            $.each(data.result.files, function (index, file) {
-                $(data.context.children()[index]).find('.circle').remove();   
 
-                if (file.url) {
-                    var link = $('<a>') .attr('target', '_blank') .prop('href', file.url);
+            }).on('fileuploadprocessalways', function (e, data) {
+                var index = data.index,
+                    file = data.files[index],
+                    node = $(data.context.children()[index]);
 
-                    //$(data.context.children()[index]).find('canvas').wrap(link);        
-                } else if (file.error) {
+                if (file.preview) {
+                    node
+                        .prepend('<br>')
+                        .prepend(file.preview);
+                }
+                if (file.error) {
+                    node
+                        .append('<br>')
+                        .append($('<span class="text-danger"/>').text(file.error));
+                }
+            }).on('fileuploadprogress', function (e, data) {
+                var progress = parseInt(data.loaded / data.total * 100, 10);
+                if (data.context) {
+                    data.context.each(function () {
+                        $(this).find('.circle').children().first().progressCircle({ 
+                                                                    nPercent : progress ,
+                                                                    showPercentText : false,
+                                                                    circleSize      : 111,
+                                                                    thickness       : 3
+                                                                });
+                    });
+                }
+            }).on('fileuploaddone', function (e, data) {
+                $.each(data.result.files, function (index, file) {
+                    $(data.context.children()[index]).find('.circle').remove();   
+
+                    if (file.url) {
+                        var link = $('<a>') .attr('target', '_blank') .prop('href', file.url);
+
+                        //$(data.context.children()[index]).find('canvas').wrap(link);        
+                    } else if (file.error) {
+                        $(data.context.children()[index])
+                            .append('<br>')
+                            .append(error);
+                    }
+
+                    if(file.name) {
+                        var hiddenName = $('<input />')
+                            .prop('id', 'imageId')
+                            .prop('type', 'hidden')
+                            .prop('name', 'ossn_photo[]')
+                            .val(file.name);
+
+                        $(data.context.children()[index]).append(hiddenName);     
+                    }
+        
+                });
+
+                // Scroll
+                $(".files").animate({ scrollLeft: $(".files")[0].scrollWidth}, 1000);
+
+            }).on('fileuploadfail', function (e, data) {
+                $.each(data.files, function (index) {
+                    $(data.context.children()[index]).find('.circle').remove();
+                    var error = $('<span class="text-danger"/>').text('File upload failed.');
                     $(data.context.children()[index])
                         .append('<br>')
                         .append(error);
-                }
+                });
+            }).prop('disabled', !$.support.fileInput)
+                .parent().addClass($.support.fileInput ? undefined : 'disabled');
 
-                if(file.name) {
-                    var hiddenName = $('<input />')
-                        .prop('id', 'imageId')
-                        .prop('type', 'hidden')
-                        .prop('name', 'ossn_photo[]')
-                        .val(file.name);
-
-                    $(data.context.children()[index]).append(hiddenName);     
-                }
-    
             });
-
-            // Scroll
-            $(".files").animate({ scrollLeft: $(".files")[0].scrollWidth}, 1000);
-
-        }).on('fileuploadfail', function (e, data) {
-            $.each(data.files, function (index) {
-                $(data.context.children()[index]).find('.circle').remove();
-                var error = $('<span class="text-danger"/>').text('File upload failed.');
-                $(data.context.children()[index])
-                    .append('<br>')
-                    .append(error);
-            });
-        }).prop('disabled', !$.support.fileInput)
-            .parent().addClass($.support.fileInput ? undefined : 'disabled');
-
-        });
 }); 
